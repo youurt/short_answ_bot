8c587636b4e8072e2487678198915e0f1593c171
youurt <ugurtigu@gmail.com>
2020-05-20 14:58:31 +0200

    make it better5

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ global TOKEN
 TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 
-# start the flask app
+
 app = Flask(__name__)
 
 """
@@ -18,36 +18,33 @@ here the route function respond to a url which is basically /{token} which is th
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-    # retrieve the message in JSON and then transform it to Telegram object
+    
     update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-    # get the chat_id to be able to respond to the same user
+   
     chat_id = update.message.chat.id
-    # get the message id to be able to reply to this specific message
+   
     msg_id = update.message.message_id
 
-    # Telegram understands UTF-8, so encode text for unicode compatibility
+    
     text = update.message.text.encode('utf-8').decode()
     print("got text message :", text)
 
-    # here we call our super AI
+
     response = answer_question(text)
 
-    # now just send the message back
-    # notice how we specify the chat and the msg we reply to
     bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
 
     return 'ok'
 
-# the route here can be anything, you the one who will call it
+
 
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
-    # we use the bot object to link the bot to our app which live
-    # in the link provided by URL
+  
     s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    # something to let us know things work
+ 
     if s:
         return "webhook setup ok"
     else:
@@ -60,6 +57,5 @@ def index():
 
 
 if __name__ == '__main__':
-    # note the threaded arg which allow
-    # your app to have more than one thread
+
     app.run(threaded=True)
